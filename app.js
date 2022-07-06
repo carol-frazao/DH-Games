5c01d0a3e13206c646e6e082f0d5a27ed0dc1ca8
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2022-07-06 12:48:48 -0300

    install firebase

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var session = require('express-session');
 var app = express();
 var bodyParser = require('body-parser');
-const { rmSync } = require('fs');
 
-/*
-Para encriptar um dado:
-let hash = bcrypt.hashSync('minhaSenha!');
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-para verificar um dado:
-bcrypt.compareSync("minhaSenha!", hash); // true
-bcrypt.compareSync("outraSenha", hash); // false
+const firebaseConfig = {
+  apiKey: "AIzaSyC_dIrAENISf1NWTtUCA0Qc27FEZP3D-yE",
+  authDomain: "dh-games-e85e6.firebaseapp.com",
+  projectId: "dh-games-e85e6",
+  storageBucket: "dh-games-e85e6.appspot.com",
+  messagingSenderId: "791205969922",
+  appId: "1:791205969922:web:b66ea01cd01b6a63bba71b",
+  measurementId: "G-6FCQB9GP4N"
+};
 
-
-middleware p adicionar dados de login
-*/
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 // view engine setup
