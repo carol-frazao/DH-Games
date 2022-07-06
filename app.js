751fe50b198405fe94879731af39c1cd11b4be0d
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2022-07-06 14:27:20 -0300

    desfiz modificações

--- app.js
+++ app.js
@@ -6,23 +6,6 @@ var session = require('express-session');
 var app = express();
 var bodyParser = require('body-parser');
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC_dIrAENISf1NWTtUCA0Qc27FEZP3D-yE",
-  authDomain: "dh-games-e85e6.firebaseapp.com",
-  projectId: "dh-games-e85e6",
-  storageBucket: "dh-games-e85e6.appspot.com",
-  messagingSenderId: "791205969922",
-  appId: "1:791205969922:web:b66ea01cd01b6a63bba71b",
-  measurementId: "G-6FCQB9GP4N"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
